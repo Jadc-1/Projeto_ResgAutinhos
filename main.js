90623266a1f3dd3c5e00d119ff5d083a7eb76537
João Alexandre <joao.ale25@gmail.com>
2025-10-26 17:40:14 -0300

    feat/main - Criei menu hamburguer responsivo para dispositivos menores

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
-// Funções para deixar o scroll automática após o click mais suave (smooth)
+/* Classe utilizada para realizar a animação de abrir e fechar o menu mobile */
 
-function irParaContatos()
-{
-    const secaoContatos = document.getElementById('contatos'); 
-    if (secaoContatos) //Permite fazer a mesma funcionalidade da tag <a> para lugares especificos
-    {
-        secaoContatos.scrollIntoView({
-            behavior: 'smooth'
+class MenuMobile {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+        this.handleClick = this.handleClick.bind(this); /*Utilizado para não perder a referência do objeto, ao usar o EventListener*/
+    }
+
+    animarLinks() {
+        this.navLinks.forEach((link, index) => {
+            /* Uso diferentes velocidades para cada link */
+
+            /* Mesma ideia de um if, utilizando um ternário para aplicar e retirar animação */
+            link.style.animation ? (link.style.animation = "") : (link.style.animation = `mainNavFade 0.5s ease forwards ${index / 7 + 0.2}s`)
         });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass)
+        this.animarLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick); /*"escuta o click" no botão e ativa a função handleClick*/
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
     }
 }
 
+const menuMobile = new MenuMobile(
+    ".menu-celular",
+    ".main-nav",
+    ".main-nav li"
+);
+
+menuMobile.init();
